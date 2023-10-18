720fc4a3c7112d7c7a6f3ec33da692358df5e530
rohittechflux <rohitb@techflux.in>
2023-10-18 13:33:12 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import BodyComponent from './Body';
 import reportWebVitals from './reportWebVitals';
 import Header from './Header';
 
@@ -10,6 +10,7 @@ root.render(
   <React.StrictMode>
 
    < Header/>
+   <BodyComponent/>
   </React.StrictMode>
 );
 
